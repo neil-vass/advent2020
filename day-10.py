86ca0f222d39c4b78b8f9b3a2945673b64fc5885
Neil Vass <neil.vass+github@gmail.com>
2020-12-12 21:24:28 +0000

    Day 10 part 2 complete

--- day-10.py
+++ day-10.py
@@ -1,3 +1,5 @@
+import itertools
+
 def test_max_for_device():
     adaptors = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
     assert max_for_device(adaptors) == 22
@@ -24,9 +26,24 @@ def test_larger_example():
     assert count_differences(0, adaptors) == { 1: 22, 3: 10 }
 
 
+def test_can_jump():
+    assert can_jump(0, 2)
+    assert not can_jump(0, 4)
+    assert not can_jump(10, 22)
+
+
+def test_count_different_arrangements_single_adaptor():
+    assert count_different_arrangements(outlet=0, adaptors=[4]) == 0
+    assert count_different_arrangements(outlet=0, adaptors=[2]) == 1
+
+
 def test_count_different_arrangements():
+    adaptors = [1,2,3]
+    assert count_different_arrangements(0, adaptors) == 4
     adaptors = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
     assert count_different_arrangements(0, adaptors) == 8
+    
+
 #-----------------------------------------------------------#
 
 def read_input(filename):
@@ -51,14 +68,46 @@ def count_differences(outlet, adaptors):
         differences[diff] = differences.get(diff, 0) + 1
     return differences
 
-def count_different_arrangements(outlet, adaptors):
-    arrangements = 0
-    adaptors = list(adaptors)
-    adaptors.sort()
+def can_jump(left, right):
+    return 1 <= (right - left) <= 3
+
+
+
+def find_arrangements(outlet_to_device):
+    diffs = [j-i for i, j in zip(outlet_to_device[:-1], outlet_to_device[1:])]
+    arrangements = 1
+    for k, g in itertools.groupby(diffs):
+        if k == 1:
+            count_of_ones = len(list(g))
+            possible_combinations = {
+                1: 1,
+                2: 2,
+                3: 4,
+                4: 7,
+                5: 13,
+                6: 22,
+            }[count_of_ones]
+            arrangements *= possible_combinations
+    return arrangements
     
 
+def count_different_arrangements(outlet, adaptors):
+    adaptors = list(adaptors)
+    adaptors.sort()
+    device = max_for_device(adaptors)
+    
+    flatter = [outlet, *adaptors, device]
+
+    for i in range(len(flatter) -1):
+        if not can_jump(flatter[i], flatter[i+1]):
+            return 0
+
+    return find_arrangements(flatter)
+
 if __name__ == "__main__":
+    # I have _no idea_ how to solve part 2, this code borrows a lot from:
+    # https://dev.to/rpalo/advent-of-code-2020-solution-megathread-day-10-adapter-array-33ea
+    # I can count combinations on my fingers and that matches the switch table, 
+    # no clue on how to make a general rule for it.
     adaptors = read_input('day-10-input.txt')
-    d = count_differences(0, adaptors)
-    print(d)
-    print(d[1] * d[3])
+    print(count_different_arrangements(0, adaptors))
