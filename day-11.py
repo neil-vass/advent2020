2ead1327cd979664a82327761524e532b8f72010
neil-vass <neil.vass+github@gmail.com>
2020-12-27 22:48:42 +0000

    DAy 11 complete

--- day-11.py
+++ day-11.py
@@ -12,6 +12,30 @@ def test_get_adjacents():
     assert get_adjacents(plan, 2, 4) == ['L', 'L', 'L', '.', '.', 'L', '.', 'L' ]
     assert get_adjacents(plan, 9, 9) == ['.', 'L', 'L']
 
+def test_get_visible_seats_wide():
+    plan = [
+        '.......#.',
+        '...#.....',
+        '.#.......',
+        '.........',
+        '..#L....#',
+        '....#....',
+        '.........',
+        '#........',
+        '...#.....'
+    ]
+    assert get_visible_seats(plan, 4, 3).count('#') == 8
+
+def test_get_visible_seats_narrow():
+    plan = [
+        '.............',
+        '.L.L.#.#.#.#.',
+        '.............'
+    ]
+    vis = get_visible_seats(plan, 1, 1)
+    assert vis.count('L') == 1
+    assert vis.count('#') == 0
+
 def test_new_state_for_pos():
     plan = get_plan('day-11-example-input.txt')
     assert new_state_for_pos(plan, 0, 0) == '#'
@@ -36,38 +60,38 @@ def test_step_change():
     
     updated = step_change(updated)
     assert updated == [
-        '#.LL.L#.##',
-        '#LLLLLL.L#',
+        '#.LL.LL.L#',
+        '#LLLLLL.LL',
         'L.L.L..L..',
-        '#LLL.LL.L#',
-        '#.LL.LL.LL',
-        '#.LLLL#.##',
+        'LLLL.LL.LL',
+        'L.LL.LL.LL',
+        'L.LLLLL.LL',
         '..L.L.....',
-        '#LLLLLLLL#',
+        'LLLLLLLLL#',
         '#.LLLLLL.L',
-        '#.#LLLL.##'
+        '#.LLLLL.L#',
     ]
     
 def test_change_until_stable():
     plan = get_plan('day-11-example-input.txt')
     assert change_until_stable(plan) == [
-        '#.#L.L#.##',
-        '#LLL#LL.L#',
-        'L.#.L..#..',
-        '#L##.##.L#',
-        '#.#L.LL.LL',
-        '#.#L#L#.##',
-        '..L.L.....',
-        '#L#L##L#L#',
-        '#.LLLLLL.L',
-        '#.#L#L#.##'
+        '#.L#.L#.L#',
+        '#LLLLLL.LL',
+        'L.L.L..#..',
+        '##L#.#L.L#',
+        'L.L#.LL.L#',
+        '#.LLLL#.LL',
+        '..#.L.....',
+        'LLL###LLL#',
+        '#.LLLLL#.L',
+        '#.L#LL#.L#',
     ]
 
 def test_count_occupied_seats():
     plan = get_plan('day-11-example-input.txt')
     assert count_occupied_seats(plan) == 0
     filled = change_until_stable(plan)
-    assert count_occupied_seats(filled) == 37
+    assert count_occupied_seats(filled) == 26
 
         
 #=======================================================#
@@ -95,15 +119,42 @@ def get_adjacents(plan, row, col):
     return adj
 
 
+def fetch_char(plan, row, col):
+        if row < 0 or row >= len(plan) or col < 0 or col >= len(plan[0]):
+            return None
+        return plan[row][col]
+
+def step_until_seat(plan, row, col, row_step, col_step):
+    next_row = row + row_step
+    next_col = col + col_step
+    ch = fetch_char(plan, next_row, next_col)
+    while ch == '.':
+        next_row += row_step
+        next_col += col_step
+        ch = fetch_char(plan, next_row, next_col)
+    return ch
+
+def get_visible_seats(plan, row, col):
+    vis = []
+    vis.append(step_until_seat(plan, row, col, -1, -1))
+    vis.append(step_until_seat(plan, row, col, -1, 0))
+    vis.append(step_until_seat(plan, row, col, -1, 1))
+    vis.append(step_until_seat(plan, row, col, 0, -1))
+    vis.append(step_until_seat(plan, row, col, 0, 1))
+    vis.append(step_until_seat(plan, row, col, 1, -1))
+    vis.append(step_until_seat(plan, row, col, 1, 0))
+    vis.append(step_until_seat(plan, row, col, 1, 1))
+    return vis
+
 def new_state_for_pos(plan, row, col):
     current = plan[row][col]
     if current == '.': 
         return '.'
 
-    adj = get_adjacents(plan, row, col)
+    adj = get_visible_seats(plan, row, col)
     if current == 'L' and adj.count('#') == 0:
         return '#'
-    elif current == '#' and adj.count('#') >= 4:
+    elif current == '#' and adj.count('#') >= 5:
         return 'L'
     else:
         return current
